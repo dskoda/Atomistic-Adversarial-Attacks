3cf1ae10d1e20870fed5f12745d5b2d6c8a1c699
Daniel Schwalbe-Koda <dskoda@mit.edu>
2023-01-23 22:06:29 -0800

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="AtomAttacks",
+    version="1.0",
+    description="Utilities for Atomistic Adversarial Attacks",
+    author="Daniel Schwalbe-Koda",
+    author_email="dskoda@llnl.gov",
+    url="https://github.com/dskoda/",
+    packages=["robust"],
+)
